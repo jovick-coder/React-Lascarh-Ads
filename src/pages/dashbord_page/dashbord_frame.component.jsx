e6226d4ddfcda30b3c6e924045b08c065b1c649a
Jovick-coder <victorjosiahm3@gmail.com>
2021-07-21 12:53:49 +0100

    made a desgin for the dashbored made it a reuseable component added icons and side nav made it mobile responsive

---
+++ src/pages/dashbord_page/dashbord_frame.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './dashbord_frame.style.css'
+import { SideNav } from '../../components/nav_bar/nav_bar.component'
+
+const DashBordFrame = ({ children, pageName }) => {
+  return (
+    <>
+      <section id="dashbord">
+        <div className="top-nav">Log</div>
+        <div className="flexbox">
+          <div className="item side-nav">
+            <div className="">
+              <SideNav />
+            </div>
+          </div>
+          <div className="item main-body">
+            <h1 className="page-name">{pageName}</h1>
+            <div className="">{children}</div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+export default DashBordFrame

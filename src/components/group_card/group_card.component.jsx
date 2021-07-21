77663117b7d17a6c728ff9d39fec1e7a1b90228c
Jovick-coder <victorjosiahm3@gmail.com>
2021-07-21 12:54:59 +0100

    designed a reusable card component

---
+++ src/components/group_card/group_card.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './group_card.style.css'
+
+const GroupCard = ({ children }) => {
+  return <div className="main-card">{children}</div>
+}
+
+export default GroupCard
